2349be5b53e1ebb54f03c44ccfc6145a128970b9
dips6202 <deepikakumari6202@gmail.com>
2022-04-01 22:11:42 +0530

    first commit

---
+++ text.py
@@ -0,0 +1,13 @@
+from json.tool import main
+import string
+
+if __name__ =="__main__":
+ s1 = string.ascii_letters
+ print(s1)
+ s2 =string.ascii_uppercase
+ print(s2)
+ s3 =string.digits
+ print(s3)
+ s4 =string.punctuation
+ print(s4)
+
