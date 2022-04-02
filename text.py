5ae516a10a463d097df7e27b5f5666c1aee963ac
dips6202 <deepikakumari6202@gmail.com>
2022-04-02 14:02:13 +0530

    change in function

--- text.py
+++ text.py
@@ -1,13 +1,23 @@
 from json.tool import main
 import string
+import random
 
 if __name__ =="__main__":
  s1 = string.ascii_letters
- print(s1)
+ 
  s2 =string.ascii_uppercase
- print(s2)
+ 
  s3 =string.digits
- print(s3)
+ 
  s4 =string.punctuation
- print(s4)
-
+ 
+ plen= int(input("enter the length of the password\n "))
+ s =[]
+ s.extend(list(s1))
+ s.extend(list(s2))
+ s.extend(list(s3))
+ s.extend(list(s4))
+ 
+ random.shuffle(s)
+ print("YOUR PASSWORD IS : ")
+ print("".join(s[0:plen]))
